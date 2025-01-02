19a7b53db73cdddc8288d71a4a3e6df16d04bbf3
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2025-01-02 12:02:16 +0200

    Task2

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,9 +1,29 @@
 class Calculator {
-  constructor() {}
+  private currentValue: number;
 
-  getCurrentValue() {}
+  constructor(initialValue: number) {
+    this.currentValue = initialValue;
+  }
 
-  action(type, value) {}
+  getCurrentValue() {
+    return this.currentValue;
+  }
+
+  action(type: 'sum' | 'multiply' | 'divide' | 'function', value: number | ((value: number) => number)) {
+    if (type === 'sum') {
+      this.currentValue += value as number;
+    } else if (type === 'multiply') {
+      this.currentValue *= value as number;
+    } else if (type === 'divide') {
+      if (value !== 0) {
+        this.currentValue /= value as number;
+      }
+    } else if (type === 'function') {
+      this.currentValue = (value as (value: number) => number)(this.currentValue);
+    } else {
+      this.currentValue = value as number;
+    }
+  }
 }
 
 export default Calculator;
