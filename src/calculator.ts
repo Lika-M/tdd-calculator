57904e647e5a7cb747316bd267b8277fca047dc2
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2025-01-02 12:12:02 +0200

    Task2 completed

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,15 +1,27 @@
 class Calculator {
   private currentValue: number;
+  private listeners: Array<(value: number) => void>;
 
   constructor(initialValue: number) {
     this.currentValue = initialValue;
+    this.listeners = [];
   }
 
   getCurrentValue() {
     return this.currentValue;
   }
 
+  addListener(listener: (value: number) => void) {
+    this.listeners.push(listener);
+  }
+
+  removeListener(listener: (value: number) => void) {
+    this.listeners = this.listeners.filter(l => l !== listener);
+  }
+
   action(type: 'sum' | 'multiply' | 'divide' | 'function', value: number | ((value: number) => number)) {
+    const previousValue = this.currentValue;
+
     if (type === 'sum') {
       this.currentValue += value as number;
     } else if (type === 'multiply') {
@@ -22,6 +34,11 @@ class Calculator {
       this.currentValue = (value as (value: number) => number)(this.currentValue);
     } else {
       this.currentValue = value as number;
+    }
+
+ 
+    if (this.currentValue !== previousValue) {
+      this.listeners.forEach(listener => listener(this.currentValue));
     }
   }
 }
