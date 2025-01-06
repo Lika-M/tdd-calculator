c0a9c911dae27476399fc75797218e22f3dcfff5
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2025-01-06 11:16:00 +0200

    Refactor Calculator to use ActionType enum and function map.

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,3 +1,10 @@
+enum ActionType {
+  Sum = "sum",
+  Multiply = "multiply",
+  Divide = "divide",
+  Function = "function",
+}
+
 class Calculator {
   private currentValue: number;
   private listeners: Array<(value: number) => void>;
@@ -19,27 +26,50 @@ class Calculator {
     this.listeners = this.listeners.filter(l => l !== listener);
   }
 
-  action(type: 'sum' | 'multiply' | 'divide' | 'function', value: number | ((value: number) => number)) {
+  // action(type: 'sum' | 'multiply' | 'divide' | 'function', value: number | ((value: number) => number)) {
+  //   const previousValue = this.currentValue;
+
+  //   if (type === 'sum') {
+  //     this.currentValue += value as number;
+  //   } else if (type === 'multiply') {
+  //     this.currentValue *= value as number;
+  //   } else if (type === 'divide') {
+  //     if (value !== 0) {
+  //       this.currentValue /= value as number;
+  //     }
+  //   } else if (type === 'function') {
+  //     this.currentValue = (value as (value: number) => number)(this.currentValue);
+  //   } else {
+  //     this.currentValue = value as number;
+  //   }
+
+  //   if (this.currentValue !== previousValue) {
+  //     this.listeners.forEach(listener => listener(this.currentValue));
+  //   }
+  // }
+
+  //  Used a map of functions to handle the different action types, making it cleaner and more maintainable.
+
+  action(type: ActionType, value: number | ((value: number) => number)): void {
     const previousValue = this.currentValue;
-
-    if (type === 'sum') {
-      this.currentValue += value as number;
-    } else if (type === 'multiply') {
-      this.currentValue *= value as number;
-    } else if (type === 'divide') {
-      if (value !== 0) {
+  
+    const actions = {
+      sum: () => (this.currentValue += value as number),
+      multiply: () => (this.currentValue *= value as number),
+      divide: () => {
+        if (value === 0) throw new Error("Cannot divide by zero.");
         this.currentValue /= value as number;
-      }
-    } else if (type === 'function') {
-      this.currentValue = (value as (value: number) => number)(this.currentValue);
-    } else {
-      this.currentValue = value as number;
-    }
-
+      },
+      function: () => (this.currentValue = (value as Function)(this.currentValue)),
+    };
+  
+    actions[type]();
+  
     if (this.currentValue !== previousValue) {
-      this.listeners.forEach(listener => listener(this.currentValue));
+      this.listeners.forEach((listener) => listener(this.currentValue));
     }
   }
+  
 }
 
 export default Calculator;
